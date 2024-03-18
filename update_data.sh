cef1601053b3a6842630e45722ea7abbcf09da14
Damien Bose <damienbose3@gmail.com>
2024-03-18 20:31:34 +0000

    Add bash script to download and pull

--- update_data.sh
+++ update_data.sh
@@ -8,7 +8,19 @@ clone_or_pull() {
     if [ -d "$folder_name" ]; then
         echo "Updating repository: $folder_name"
         cd "$folder_name"
+        git checkout master
+
+        # Deletes the reflog (to save space)
+        git reflog expire --expire=now --all
+        git gc --aggressive --prune=now
+
+        # Update with latest
         git pull
+
+        # Deletes the reflog (to save space)
+        git reflog expire --expire=now --all
+        git gc --aggressive --prune=now
+
         cd ..
     else
         echo "Cloning repository: $folder_name"
@@ -25,13 +37,13 @@ cd "$target_folder"
 
 # List of repositories
 repositories=(
-    https://github.com/COMP0087-OpenNLP/gte-large
     https://github.com/COMP0087-OpenNLP/voyage
     https://github.com/COMP0087-OpenNLP/cohere
     https://github.com/COMP0087-OpenNLP/sentences
     https://github.com/COMP0087-OpenNLP/llmrails
     https://github.com/COMP0087-OpenNLP/gist
     https://github.com/COMP0087-OpenNLP/angle
+    https://github.com/COMP0087-OpenNLP/gte-large
 )
 
 # Clone or pull the repositories
