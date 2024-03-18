20a4a926ea9436091cd86cbaad59fd193aa2df2f
Damien Bose <damienbose3@gmail.com>
2024-03-18 20:34:47 +0000

    Remove git submodules and use list of repos

--- update_data.sh
+++ update_data.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# List of data repositories
+repositories=(
+    https://github.com/COMP0087-OpenNLP/voyage
+    https://github.com/COMP0087-OpenNLP/cohere
+    https://github.com/COMP0087-OpenNLP/sentences
+    https://github.com/COMP0087-OpenNLP/llmrails
+    https://github.com/COMP0087-OpenNLP/gist
+    https://github.com/COMP0087-OpenNLP/angle
+    https://github.com/COMP0087-OpenNLP/gte-large
+)
+
 # Function to clone or pull a repository
 clone_or_pull() {
     repo_url=$1
@@ -34,17 +45,6 @@ target_folder="experimentation/data"
 # Create the target folder if it doesn't exist
 mkdir -p "$target_folder"
 cd "$target_folder"
-
-# List of repositories
-repositories=(
-    https://github.com/COMP0087-OpenNLP/voyage
-    https://github.com/COMP0087-OpenNLP/cohere
-    https://github.com/COMP0087-OpenNLP/sentences
-    https://github.com/COMP0087-OpenNLP/llmrails
-    https://github.com/COMP0087-OpenNLP/gist
-    https://github.com/COMP0087-OpenNLP/angle
-    https://github.com/COMP0087-OpenNLP/gte-large
-)
 
 # Clone or pull the repositories
 for repo in "${repositories[@]}"; do
