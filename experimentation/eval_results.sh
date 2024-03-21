53e0afceb451cfe1b3694290661710fee14839ff
Suhail <suhailm0608@gmail.com>
2024-03-21 12:28:53 +0000

    Results so far

--- experimentation/eval_results.sh
+++ experimentation/eval_results.sh
@@ -1,2 +1,5 @@
+result_dir=damien_temp_logs
+rm -rf $result_dir
+mkdir -p $result_dir
 jupyter nbconvert --to script snlp_test.ipynb
-python3 snlp_test.py 
+python3 snlp_test.py &> $result_dir/mteb_eval_logs.txt 2>&1 &
