54af283f6c072491428d72b473482f33d34550f3
Damien Bose <damienbose3@gmail.com>
2024-04-05 09:37:10 +0100

    eval with tmux

--- experimentation/eval_results.sh
+++ experimentation/eval_results.sh
@@ -1,5 +1,2 @@
-result_dir=eval_logs
-rm -rf $result_dir
-mkdir -p $result_dir
 jupyter nbconvert --to script snlp_test.ipynb
-python3 snlp_test.py &> $result_dir/mteb_eval_logs.txt 2>&1 &
+python3 snlp_test.py
